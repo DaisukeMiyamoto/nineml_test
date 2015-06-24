e987ad40801fbda34158e52eb94d98e4ac6b59e2
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2015-06-25 02:28:03 +0900

    change form to class

--- nineml/dcn_test.py
+++ nineml/dcn_test.py
@@ -5,117 +5,104 @@ if simulator == 'neuron':
     from pype9.cells.neuron import CellMetaClass  # @UnusedImport
 else:
     from pype9.cells.nest import CellMetaClass  # @Reimport
-
 import os.path
-#import unittest
-'''
-from nineml import load, Document
-from nineml.user.multicomponent import (
-    MultiComponent, SubComponent, PortExposure, MultiCompartment,
-    FromSibling, FromDistal, FromProximal, Mapping, Domain)
-from nineml.abstraction import (
-    Dynamics, Regime, AnalogReceivePort, OutputEvent, AnalogSendPort, On,
-    StateAssignment)
-from nineml.user.port_connections import PortConnection
-from nineml.user.component import DynamicsProperties
-'''
 from neuron import h
 
+class nineml_multicompartment:
+    def __init__(self, mc):
+        self.pc = h.ParallelContext()
+        self.mc = mc
+        self.tree = mc.tree
+        self.name = mc.name
+        self.sections = []
+        self.section_def_template = 'create %s[%d]'
+        self.complexity = 0
+        self.setup_time = 0
+        self.calc_time = 0
+        self.num_compartment = 0
 
-def insert_domain(mc, sec, domain):
-    modlist = []
-    for subcomp in domain.dynamics.subcomponents:
-        modlist.append(subcomp.name)
-        sec.insert(subcomp.name)
-    #print modlist
-    #for domain in mc.domains:
-    #    print domain
-    #    for comp in domain.dynamics.subcomponents:
-            
+    def insert_domain(self, sec, domain):
+        modlist = []
+        for subcomp in domain.dynamics.subcomponents:
+            modlist.append(subcomp.name)
+            sec.insert(subcomp.name)
+        #print modlist
+        #for domain in mc.domains:
+        #    print domain
+        #    for comp in domain.dynamics.subcomponents:
 
-def setup_sections(mc):
-    tree = mc.tree
-    pc = h.ParallelContext()
-
-    ##############################################################
-    # show mechanisms
-    h.chk_mcomplex()
-
-    ##############################################################
-    # set up sections
-    sections = []
-    section_def_template = 'create %s[%d]'
-    h(section_def_template % (mc.name, len(tree)))
-    for sec in h.allsec():
-        sections.append(sec)
-    #for i in range(len(tree)):
-    #    sections.append(h.Section(mc.comp1.name+'['+str(i)+']'))
-
-
-    ##############################################################
-    # connect sections
-    for i,sec in enumerate(sections):
-        parent = tree[i]
-        #print "%d to %d" % (i, tree[i])
-        if(parent != 0):
-            sec.connect(sections[parent-1], 1, 0)
-
-    ##############################################################
-    # insert phisiology
-    for i,sec in enumerate(sections):
-        #print "set %s to %d" % (mc.mapping.domain_name(i), i)
-        insert_domain(mc, sec, mc.domain(i))
+    def check_complexity_file(self):
+        # show mechanisms
+        h.chk_mcomplex()
         
-    #for domain in mc.domains:
-    #    for comp in domain.dynamics.subcomponents:
-            
-            
-            
-    #for i,sec in enumerate(sections):
-        #sec.insert('hh')
-        #print mc.comp1.domain(i)
-        #if(mc.comp1.mapping[1][i]==0):
-        #    sec.insert('hh')
-        #else:
-        #    sec.insert('pas')
-
-    ##############################################################
-    # show topology    
-    if(pc.id()==0):
-        print tree
-        #h.topology()
+    def setup_sections(self):
+        start = h.startsw()
+        
+        ###################################################
+        # set up sections
+        self.sections = []
+        # for section name in hoc
+        h(self.section_def_template % (self.name, len(self.tree)))
+        for sec in h.allsec():
+            self.sections.append(sec)
 
 
-    ##############################################################
-    # test split
-    cplx = h.multisplit()
-    num_cmp = 0
-    for sec in h.allsec():
-        num_cmp += 1
+        # connect sections
+        for i,sec in enumerate(self.sections):
+            parent = self.tree[i]
+            #print "%d to %d" % (i, tree[i])
+            if(parent != 0):
+                sec.connect(self.sections[parent-1], 1, 0)
+
+        ##############################################################
+        # insert phisiology
+        for i,sec in enumerate(self.sections):
+            #print "set %s to %d" % (mc.mapping.domain_name(i), i)
+            self.insert_domain(sec, self.mc.domain(i))
+        
+        #for domain in mc.domains:
+        #    for comp in domain.dynamics.subcomponents:
+        
+        ##############################################################
+        # split
+        self.complexity = h.multisplit()
+        for sec in h.allsec():
+            self.num_compartment += 1
+
+        # debug
+        print self.tree
+
+        self.setup_time += h.startsw() - start
 
 
-    ##############################################################
-    # show topology and result
-    setup_time = 0
-    calc_time = 0
-    if(pc.id()==0):
-        print "\n##############################################################"
-        print "# setup time = %.2f sec" % setup_time
-        print "# calc time = %.2f sec" % calc_time
+    def run_simulation(self):
+        return 0.0
+
+    def show_info(self):
+
+        if(self.pc.id()==0):
+            print "\n##############################################################"
+            print "# setup time = %.2f sec" % self.setup_time
+            print "# calc time = %.2f sec" % self.calc_time
+            print "#"
+
+
+        for i in range(int(self.pc.nhost())):
+            self.pc.barrier()
+            if(i==self.pc.id()):
+                print "# %d/%d : %d compartment (%d)" % (self.pc.id(), self.pc.nhost(), self.num_compartment, self.complexity)
         print "#"
 
-
-    for i in range(int(pc.nhost())):
-        pc.barrier()
-        if(i==pc.id()):
-            print "# %d/%d : %d compartment (%d)" % (pc.id(), pc.nhost(), num_cmp, cplx)
-
-    print "#"
-    #for i in range(int(pc.nhost())):
-    #    pc.barrier()
-    #    if(i==pc.id()):
-    #        h.topology()
-
+    def show_topology(self, id):
+        if id < 0:
+            for i in range(int(self.pc.nhost())):
+                pc.barrier()
+                if(i==pc.id()):
+                    h.topology()
+        else:
+            if(id == self.pc.id()):
+                h.topology()
+        
 
 def main():
     h('{nrn_load_dll("/home/nebula/git/CerebellarNuclei/x86_64/.libs/libnrnmech.so")}')
@@ -125,13 +112,14 @@ def main():
         os.environ['HOME'], 'git', 'CerebellarNuclei', '9ml',
         'dcn.xml'))['DCN']
     
+    mc = nineml_multicompartment(dcn)
     #dcn_cell = CellMetaClass(dcn)
-    setup_sections(dcn)
-    #runsimulation()
-    #show_info()
+
+    mc.check_complexity_file()
+    mc.setup_sections()
+    mc.run_simulation()
+    mc.show_info()
 
 
 if __name__ == '__main__':
     main()
-    #mc = TestMultiCompartment()
-    #xml = mc.test_multicompartment_xml()
